7cfa1886a3df2219c5455ae7b3bb69b2e70a1cca
aonai <syuxiaolai@gmail.com>
2021-01-25 23:35:16 -0600

    vision - tracking pen and drawing on frame

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ This project is going classify words written in air in front of a depth camera.
 * [PyTorch](https://pytorch.org/get-started/locally/)
 * [EMNIST](https://www.nist.gov/itl/products-and-services/emnist-dataset) - install using `pip install emnist`
 * [OpenCV](https://opencv.org/#)
+* [pyrealsense2](https://intelrealsense.github.io/librealsense/python_docs/_generated/pyrealsense2.html)
+* [Intel`&reg;`RealSense`&trade;` Depth Camera D435i](https://www.intelrealsense.com/depth-camera-d435i/)
--- pen_tracker.py
+++ pen_tracker.py
@@ -1,65 +1,165 @@
-import cv2 as cv
+import pyrealsense2 as rs
 import numpy as np
-import time
+import cv2 as cv
+
+# ------------- variables ---------------
+frame_width = 640
+frame_height = 480
+kernel = np.ones((5,5),np.uint8)
+
+# ------------ members for streaming -----------
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.depth, frame_width, frame_height, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, frame_width, frame_height, rs.format.bgr8, 30)
+profile = pipeline.start(config)
 
 
-COLOR_WINDOW_NAME = "Find Pen Color"
+class Handler():
+    """ helper class to handle realsen camera vision """
+    def __init__(self):
+        self.background_color = 255
+        self.clipping_distance_in_meters = 0.3
+        self.min_distance_in_meters = -0.3
 
+        self.pen = [None, None]
+        self.canvas = None
 
-def emptyFunction(x):
-    """ helper empty function for trackbars """
-    pass
+        self.window_name = "Track Pen"
+        self.window_images = None
 
-def init_color_window():
-    cv.namedWindow(COLOR_WINDOW_NAME)
-    cv.createTrackbar('Lower H', COLOR_WINDOW_NAME, 110, 180, emptyFunction) 
-    cv.createTrackbar('Lower S', COLOR_WINDOW_NAME, 120, 255, emptyFunction) 
-    cv.createTrackbar('Lower V', COLOR_WINDOW_NAME, 120, 255, emptyFunction)
-    cv.createTrackbar('Upper H', COLOR_WINDOW_NAME, 130, 180, emptyFunction) 
-    cv.createTrackbar('Upper S', COLOR_WINDOW_NAME, 255, 255, emptyFunction) 
-    cv.createTrackbar('Upper V', COLOR_WINDOW_NAME, 255, 255, emptyFunction)
+        self.setup_stream()
+        cv.namedWindow(self.window_name, cv.WINDOW_AUTOSIZE)
+        # find range of color to filter 
+        # self.enable_trackbar() 
+        while True:
+            self.setup_window()
+            cv.imshow(self.window_name, self.window_images)
+
+            # ---------  keys ----------------
+            key = cv.waitKey(1)
+            if key & 0xFF == ord('q') or key == 27:
+                # Press esc or 'q' to close the image window
+                cv.destroyAllWindows()
+                break
+                
+    def setup_stream(self):
+        # setup streaming 
+        depth_sensor = profile.get_device().first_depth_sensor()
+        depth_scale = depth_sensor.get_depth_scale()
+        print("Depth Scale is: " , depth_scale)
+
+        self.clipping_distance = self.clipping_distance_in_meters / depth_scale
+        self.min_distance = self.min_distance_in_meters / depth_scale
+
+        # align depth to color frames
+        align_to = rs.stream.color
+        self.align = rs.align(align_to)
+
+    def enable_trackbar(self):
+        cv.createTrackbar('Lower H', self.window_name, 110, 180, lambda _: None) 
+        cv.createTrackbar('Lower S', self.window_name, 120, 255, lambda _: None) 
+        cv.createTrackbar('Lower V', self.window_name, 120, 255, lambda _: None)
+        cv.createTrackbar('Upper H', self.window_name, 130, 180, lambda _: None) 
+        cv.createTrackbar('Upper S', self.window_name, 255, 255, lambda _: None) 
+        cv.createTrackbar('Upper V', self.window_name, 255, 255, lambda _: None)
+
+    def get_tracekbar_val(self):
+        lH = cv.getTrackbarPos('Lower H', self.window_name)
+        lS = cv.getTrackbarPos('Lower S', self.window_name)
+        lV = cv.getTrackbarPos('Lower V', self.window_name)
+        uH = cv.getTrackbarPos('Upper H', self.window_name)
+        uS = cv.getTrackbarPos('Upper S', self.window_name)
+        uV = cv.getTrackbarPos('Upper V', self.window_name)
+        
+        lower_color = np.array([lH,lS,lV])
+        upper_color = np.array([uH,uS,uV])
+
+        # red range
+        lower_color = np.array([135,110,50])
+        upper_color = np.array([180,250,150])
+
+        return lower_color, upper_color
+        
+    def setup_window(self):
+        # get frames 
+        frames = pipeline.wait_for_frames()
+        aligned_frames = self.align.process(frames)
+        aligned_depth_frame = aligned_frames.get_depth_frame() 
+        color_frame = aligned_frames.get_color_frame()
+
+        if not aligned_depth_frame or not color_frame: 
+            raise Exception("WARNING: Frame not found")
+
+        # get camera images
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+        
+        if self.canvas is None:
+            self.canvas = np.zeros_like(color_image)
+
+        depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
+        bg_removed = np.where((depth_image_3d > self.clipping_distance) | (depth_image_3d <= self.min_distance), \
+                            self.background_color, color_image)
+             
+        # filter red coler
+        hsv = cv.cvtColor(bg_removed, cv.COLOR_BGR2HSV)
+        mask = self.filter_color(hsv)
+
+        # draw
+        color_image = self.draw(mask, color_image)
+
+        mask_bw = cv.cvtColor(mask, cv.COLOR_GRAY2BGR) # back and white images 
+        res = cv.bitwise_and(bg_removed, bg_removed, mask=mask)
+
+        # depth and color images combined 
+        depth_colormap = cv.applyColorMap(cv.convertScaleAbs(depth_image, alpha=0.03), cv.COLORMAP_JET) 
+        
+        # stack images
+        self.window_images = np.hstack((bg_removed, color_image, res))
+    
+    def filter_color(self, hsv):
+        lower_color, upper_color = self.get_tracekbar_val()
+        mask = cv.inRange(hsv, lower_color, upper_color)
+        # filter white nosie
+        # https://stackoverflow.com/questions/42272384/opencv-removal-of-noise-in-image
+        mask = cv.dilate(mask, kernel, iterations = 1)
+        mask = cv.erode(mask, kernel, iterations = 1)
+        mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
+        return mask
+
+    def draw(self, mask, image):
+        """
+        Draw rectangle around pen and save location of pen tip
+            mask - image mask that is filter by a choosen color
+            image - image to draw contours on
+        """
+        contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        if contours and cv.contourArea(max(contours, key=cv.contourArea)) > 200:
+            c = max(contours, key=cv.contourArea)
+            x, y, w, h = cv.boundingRect(c)
+            cv.rectangle(image, (x, y), (x+w, y+h), (0,25, 255), 2)
+
+            # TODO: filter noises here 
+            self.pen[0] = self.pen[1]
+            self.pen[1] = (x, y)
+        
+        if not self.pen[0] is None:
+            self.canvas = cv.line(self.canvas, self.pen[0], self.pen[1], (0,25, 255), 5)
+            image = cv.add(image, self.canvas)
+
+        return image
+        
+
 
 def main():
-    cap = cv.VideoCapture(0)
-    if not cap.isOpened():
-        raise IOError("Cannot open webcam")
-
-    init_color_window()
-
-    while True:
-        ret, frame = cap.read()
-        if not cap.isOpened():
-            raise IOError("Cannot open webcam")
-        
-        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-
-        lowerH = cv.getTrackbarPos('Lower H', COLOR_WINDOW_NAME) 
-        lowerS = cv.getTrackbarPos('Lower S', COLOR_WINDOW_NAME) 
-        lowerV = cv.getTrackbarPos('Lower V', COLOR_WINDOW_NAME) 
-        upperH = cv.getTrackbarPos('Upper H', COLOR_WINDOW_NAME) 
-        upperS = cv.getTrackbarPos('Upper S', COLOR_WINDOW_NAME) 
-        upperV = cv.getTrackbarPos('Upper V', COLOR_WINDOW_NAME) 
-        lower = np.array([lowerH,lowerS,lowerV])
-        upper = np.array([upperH,upperS,upperV])
-
-        mask = cv.inRange(hsv, lower, upper)
-        res = cv.bitwise_and(frame,frame, mask= mask)
-        mask_3 = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
-
-        stacked = np.hstack((mask_3,frame,res))
-        cv.imshow(COLOR_WINDOW_NAME, stacked)
-
-        key = cv.waitKey(1)
-        if key == 27: # ESC 
-            break
-        elif key == ord('s'):
-            savedColor = np.array([[lowerH,lowerS,lowerV],[upperH, upperS, upperV]])
-            print("Found pen color: ", savedColor)
-            break
-
-    cap.release()
-    cv.destroyAllWindows()
-    
+    """ The main() function. """
+    handler = Handler()
 
 if __name__=="__main__": 
-    main()
+    try:
+        main()
+    except Exception as inst:
+            print(inst)
+    finally:
+        pipeline.stop()
